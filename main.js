8d5c9edfb2280daffd7189287cbea083914cbee1
qudlaty <qudlaty@users.noreply.github.com>
2019-11-04 00:24:35 +0100

    messenger fix

---
+++ main.js
@@ -0,0 +1,12 @@
+//messenger plugin code
+(function () {
+        var options = {
+            facebook: "102180314567834", // Facebook page ID
+            call_to_action: "Wyślij wiadomaść", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
